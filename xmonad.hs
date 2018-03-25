af8e40c59b2c70f03dcd3f3551543edb925e0182
Jarosław Jantura <jjantura@gmail.com>
2018-03-25 18:47:21 +0200

    Add default config

---
+++ xmonad.hs
@@ -0,0 +1,10 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import Data.Default
+
+main = do
+    xmonad $ def
